8b87b5b097cc68352a400e02d98dfe51aa0ee35b
Arjun Saxena <saxenaarjun1239@gmail.com>
2024-10-15 10:30:20 +0530

    first JS file

---
+++ index.js
@@ -0,0 +1,19 @@
+// console.log("Hello js");
+
+// const a=12;
+// if(a>10){
+//     let b=23;
+//     a=40
+//     console.log("a="+a);
+// }
+// console.log("Hi, a="+a);
+
+function fun(a,b){
+    console.log("HEEEE");
+    return a+b;
+}
+fun();
+let data1=fun();
+console.log(data1);
+let data=fun(10,2);
+console.log(data)
